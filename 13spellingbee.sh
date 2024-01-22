3c221d11a2a32418a87811ff0b4b999f53f3d6d5
username <apanigrahi@ucdavis.edu>
2024-01-21 21:20:50 -0800

    co-author

--- 13spellingbee.sh
+++ 13spellingbee.sh
@@ -1,2 +1,3 @@
+# co-author: Aman, Ashley
 gunzip -c dictionary.gz | grep "r" | grep -v "[bdefghjklmpqstuvwxy]" | grep -E ".{3}."
 # gunzip dictionary.gz -c | grep -xE "[ziacorn]{4,}" | grep "r"  this is what the TA and I came up with in office hours 
