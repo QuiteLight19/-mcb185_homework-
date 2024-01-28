019e51fbf0f74036e03d03fda65706a0017beb8d
username <apanigrahi@ucdavis.edu>
2024-01-27 22:57:19 -0800

    a

--- 22oligotemp.py
+++ 22oligotemp.py
@@ -5,8 +5,13 @@ import math
 
 def oligotemp(a, c, g, t):
 	total = (a + c + g + t)
-	if total <= 13: return ((a + t)* 2) + ((g + c)* 4)
-	elif total > 13: return (64.9 + 41* ((g+c - 16.4) / (total)))
+	if type(a) != int or type(c) != int or type (g) != int or type(t) != int:
+		return 'error: must be whole numbers'
+	elif total <= 0 : return 'total has to be greater than 0'
+	elif total <= 13: return ((a + t) * 2 + (g + c) * 4)
+	elif total >  13: return (64.9 + (41 * (g + c - 16.4) / (a + c + g + t)))
+	else: return 'not possible'
+print(oligotemp(-0.1, -1, -1, -1))
 print(oligotemp(10, 10, 10, 10))
 print(oligotemp(23, 45, 1, 3))
 print(oligotemp(1, 1, 1, 1))
