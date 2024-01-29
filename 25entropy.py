5b2e4df894752b59af0aa06e852661b6edb3a719
username <apanigrahi@ucdavis.edu>
2024-01-28 21:20:26 -0800

    added

--- 25entropy.py
+++ 25entropy.py
@@ -1,5 +1,5 @@
 # 25entropy by Aman Panigrahi
-# co-authors: Aman, David
+# co-authors: Aman, David, Ashley
 import math
 
 def shannonentropy(a, c, g, t):	
@@ -13,7 +13,7 @@ def shannonentropy(a, c, g, t):
 	elif a < 0 or c < 0 or g < 0 or t < 0:
 		return 'must be positive'
 	else:
-		return -(pa * math.log2(pa) + pc * math.log2(pc) + pg * math.log2(pg) + pt * math.log2(pt))
+		return -(pa*math.log2(pa) + pc*math.log2(pc) + pg*math.log2(pg) + pt*math.log2(pt))
 print(shannonentropy(1, 9, 2, 3))
 print(shannonentropy(12, 452, 235, 134))
 print(shannonentropy(-3, 4, 3, 98))
