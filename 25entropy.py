91f37130ae73f6924a8198b167865e35ac12740d
username <apanigrahi@ucdavis.edu>
2024-01-29 11:21:26 -0800

    added

--- 25entropy.py
+++ 25entropy.py
@@ -8,12 +8,12 @@ def shannonentropy(a, c, g, t):
 	pc = (c / nt)
 	pg = (g / nt)
 	pt = (t / nt)
-	if type(a) != int or type(c) != int or type (g) != int or type(t) != int:
-		return 'error: values must be integers'
-	elif a < 0 or c < 0 or g < 0 or t < 0:
-		return 'must be positive'
-	else:
-		return -(pa*math.log2(pa) + pc*math.log2(pc) + pg*math.log2(pg) + pt*math.log2(pt))
+	entropy = 0
+	if a > 0: entropy = entropy + pa*math.log2(pa)
+	if c > 0: entropy = entropy + pc*math.log2(pc)
+	if g > 0: entropy = entropy + pg*math.log2(pg)
+	if t > 0: entropy = entropy + pt*math.log2(pt)
+	return -entropy
 print(shannonentropy(1, 9, 2, 3))
 print(shannonentropy(12, 452, 235, 134))
-print(shannonentropy(-3, 4, 3, 98))
+print(shannonentropy(0, 4, 3, 98))
