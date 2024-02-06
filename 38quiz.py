f66baecb97e61cc9ee3d3f216981e61086cc5c0f
username <apanigrahi@ucdavis.edu>
2024-02-06 09:30:38 -0800

    added

---
+++ 38quiz.py
@@ -0,0 +1,19 @@
+#Co- Authors: Aman, Ashley
+pia = 1
+na = 1
+signa = -1
+
+for i in range(1, 201, 2):
+	pia = pia + signa*(1/(na+2))
+	na = na + 2
+	signa =-signa
+	print(4 * pia)
+	pib = 3
+	nb = 2
+	signb = 1
+	for j in range(100):
+		pib = pib + signb * (4/ ((nb) * (nb+1) * (nb+2)))
+		nb = nb + 2
+		signb = signb * (-1)
+		print(pia, pib)
+		
