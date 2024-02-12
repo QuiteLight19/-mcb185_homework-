8d22dc662914a76b3cd4a97761aa9f9530894aac
username <apanigrahi@ucdavis.edu>
2024-02-11 16:24:50 -0800

    added

--- 47deathsaves.py
+++ 47deathsaves.py
@@ -13,10 +13,10 @@ for i in range(games):
 	r = 0
 	while f < 3 and s < 3 and r < 1:
 		d = random.randint(1, 20)
-		if d == 20: r += 1
-		if d < 20 and d >= 10: s += 1
-		if d < 10 and d >= 2: f += 1
-		if d == 1: f += 2
+		if   d == 20: r += 1
+		elif d <  20 and d >= 10: s += 1
+		elif d <  10 and d >=  2: f += 1
+		else					: f += 2
 		if f >= 3: die += 1
 		if r == 1: revive += 1
 		if s == 3: stable +=1
